616ff435392a9c57a04710cf70faba14395426ef
Gagan Deep Singh <me@gagan.pro>
2017-08-25 23:34:02 +0530

    Conditional declaration of constants so as to prevent white screen of death

--- image-quality.php
+++ image-quality.php
@@ -4,7 +4,7 @@
   Plugin Name: Image Quality
   Plugin URI:  https://wordpress.org/plugins/image-quality/
   Description: Plugin to let you adjust compression ratio WordPress uses in media settings
-  Version:     1.4
+  Version:     1.5
   Author:      Gagan Deep Singh
   Author URI:  https://gagan0123.com
   License:     GPLv2
@@ -18,9 +18,40 @@ if ( !defined( 'ABSPATH' ) ) {
 	die;
 }
 
-define( 'GS_IQ_PREFIX', 'gs_iq' );
-define( 'GS_IQ_PATH', trailingslashit( plugin_dir_path( __FILE__ ) ) );
-define( 'GS_IQ_SETTINGS_SLUG', GS_IQ_PREFIX . '_image_quality' );
+if ( !defined( 'GS_IQ_PREFIX' ) ) {
+	/**
+	 * Prefix for most of the things in this plugin
+	 * 
+	 * @since 1.0
+	 */
+	define( 'GS_IQ_PREFIX', 'gs_iq' );
+}
+if ( !defined( 'GS_IQ_PATH' ) ) {
+	/**
+	 * Path to the plugin directory
+	 * 
+	 * @since 1.0
+	 */
+	define( 'GS_IQ_PATH', trailingslashit( plugin_dir_path( __FILE__ ) ) );
+}
+
+if ( !defined( 'GS_IQ_SETTINGS_SLUG' ) ) {
+	/**
+	 * Settings slug
+	 * 
+	 * @since 1.0
+	 */
+	define( 'GS_IQ_SETTINGS_SLUG', GS_IQ_PREFIX . '_image_quality' );
+}
+
+if ( !defined( 'GS_IQ_BASENAME' ) ) {
+	/**
+	 * Basename of the plugin
+	 * 
+	 * @since 1.5
+	 */
+	define( 'GS_IQ_BASENAME', plugin_basename( __FILE__ ) );
+}
 
 /**
  * The core plugin class
